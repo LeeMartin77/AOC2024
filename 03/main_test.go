e737b8ad3665b07942d05cf6ac5fd65e301b6d18
Lee <hello@leejohnmartin.co.uk>
2024-12-03 08:19:30 +0000

    feat: Day 03 sol 02

--- 03/main_test.go
+++ 03/main_test.go
@@ -13,13 +13,8 @@ func TestPhaseOne(t *testing.T) {
 	assert.Equal(t, int64(161), res)
 }
 
-// func TestPhaseTwo(t *testing.T) {
-// 	teststring := `7 6 4 2 1
-// 1 2 7 8 9
-// 9 7 6 2 1
-// 1 3 2 4 5
-// 8 6 4 4 1
-// 1 3 6 7 9`
-// 	res := solution.ComputeSolutionTwo([]byte(teststring))
-// 	assert.Equal(t, int64(4), res)
-// }
+func TestPhaseTwo(t *testing.T) {
+	teststring := `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`
+	res := solution.ComputeSolutionTwo([]byte(teststring))
+	assert.Equal(t, int64(48), res)
+}
--- 03/solution/solution.go
+++ 03/solution/solution.go
@@ -25,5 +25,31 @@ func ComputeSolutionOne(data []byte) int64 {
 }
 
 func ComputeSolutionTwo(data []byte) int64 {
-	panic("unimplemented")
+	str := string(data)
+	r, err := regexp.Compile(`mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)`)
+	if err != nil {
+		panic(err)
+	}
+	acc := int64(0)
+	enabled := true
+	for _, mulop := range r.FindAllString(str, -1) {
+		opsdig := strings.Split(mulop, "(")
+		op, digits := opsdig[0], opsdig[1]
+		if op == "don't" {
+			enabled = false
+			continue
+		}
+		if op == "do" {
+			enabled = true
+			continue
+		}
+		if op == "mul" && enabled {
+			digits = digits[:len(digits)-1]
+			digits_split := strings.Split(digits, ",")
+			a, _ := strconv.ParseInt(digits_split[0], 10, 64)
+			b, _ := strconv.ParseInt(digits_split[1], 10, 64)
+			acc += (a * b)
+		}
+	}
+	return acc
 }
