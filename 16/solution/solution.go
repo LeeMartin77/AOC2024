135d3aa8379af90a39fc7f5026c7f9ac3c2d9f91
Lee <hello@leejohnmartin.co.uk>
2024-12-16 11:31:58 +0000

    feat: Day 16 sol 01
    We apparently solve this by just... fuzzing it

--- 16/solution/solution.go
+++ 16/solution/solution.go
@@ -126,16 +126,16 @@ func (rnd reindeer) GenerateFuturePossibleReindeer(maze map[int]map[int]bool) []
 	return ftr
 }
 
-func recurrFlock(maze map[int]map[int]bool, exit []int, flock []reindeer, current_low_score int) int {
+func recurrFlock(maze map[int]map[int]bool, exit []int, flock []reindeer, current_low_score reindeer) reindeer {
 	if len(flock) == 0 {
 		return current_low_score
 	}
 	remaining := []reindeer{}
 	for _, rnd := range flock {
-		if rnd.Position[0] == exit[0] && rnd.Position[1] == exit[1] && current_low_score > rnd.Score {
-			current_low_score = rnd.Score
+		if rnd.Position[0] == exit[0] && rnd.Position[1] == exit[1] && current_low_score.Score > rnd.Score {
+			current_low_score = rnd
 		}
-		if rnd.Score < current_low_score {
+		if rnd.Score < current_low_score.Score {
 			remaining = append(remaining, rnd)
 		}
 	}
@@ -162,7 +162,23 @@ func recurrFlock(maze map[int]map[int]bool, exit []int, flock []reindeer, curren
 	wg.Wait()
 	close(ftrchn)
 	<-dnchn
-	return recurrFlock(maze, exit, future, current_low_score)
+	// take the lowest from flock for each position
+	// - basically, if we end up in the same place from two reindeer, only
+	// the lowest survives
+	fastest := []reindeer{}
+	for _, rnd := range future {
+		other := slices.IndexFunc(fastest, func(f reindeer) bool {
+			return (f.Position[0] == rnd.Position[0] && f.Position[1] == rnd.Position[1]) || slices.ContainsFunc(f.History, func(hst []int) bool {
+				return hst[0] == rnd.Position[0] && hst[1] == rnd.Position[1]
+			})
+		})
+		if other == -1 {
+			fastest = append(fastest, rnd)
+		} else if fastest[other].Score > rnd.Score {
+			fastest[other] = rnd
+		}
+	}
+	return recurrFlock(maze, exit, fastest, current_low_score)
 }
 
 func ComputeSolutionOne(data []byte) int64 {
@@ -172,9 +188,15 @@ func ComputeSolutionOne(data []byte) int64 {
 
 	flock := []reindeer{NewReindeer(start, []int{1, 0}, 0, 1000, 1, [][]int{start})}
 
-	low_score := recurrFlock(maze, exit, flock, current_low_score)
+	low_score := reindeer{Score: current_low_score}
+	for range 10 {
+		poss_low_score := recurrFlock(maze, exit, flock, reindeer{Score: current_low_score})
+		if poss_low_score.Score < low_score.Score {
+			low_score = poss_low_score
+		}
+	}
 
-	return int64(low_score)
+	return int64(low_score.Score)
 }
 
 func ComputeSolutionTwo(data []byte) int64 {
